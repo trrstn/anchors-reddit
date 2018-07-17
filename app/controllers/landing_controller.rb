ad37aa0a5f7dc4de73eb3d9fd49a11bca0885643
Humphrey <humphrey.litan@gmail.com>
2018-07-17 18:49:03 +0800

    displayed posts in the landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,7 @@
 class LandingController < ApplicationController
 
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_063925) do
+ActiveRecord::Schema.define(version: 2018_07_16_090101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,19 @@ ActiveRecord::Schema.define(version: 2018_07_16_063925) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
+    t.string "password_hash"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "posts", "users"
 end
