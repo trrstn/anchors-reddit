3a2844481a868666de7f9851fa9c6abe00a1d8c3
Humphrey <humphrey.litan@gmail.com>
2018-07-18 12:50:49 +0800

    change header elements if the user is logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,16 @@ class SessionsController < ApplicationController
 
     if(user && user.authenticate(params[:password]))
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to landing_path
     else
-      redirect_to '/login'
+      redirect_to login_path
     end
 
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to landing_path
   end
 
 end
