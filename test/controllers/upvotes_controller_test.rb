1bcf2463c6b9be3f461dcb20ff7b950d8385246f
Tristan <trrstnm@gmail.com>
2018-07-20 19:19:28 +0800

    removed tests

--- test/controllers/upvotes_controller_test.rb
+++ test/controllers/upvotes_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class UpvotesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get upvotes_create_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get upvotes_destroy_url
-    assert_response :success
-  end
 
 end
