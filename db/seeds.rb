25d99229937d65b7a2f250291192d763c6c8ca80
Tristan <trrstnm@gmail.com>
2018-07-26 18:22:48 +0800

    added super admin on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-john = User.create({first_name: 'John', last_name: 'Doe', user_name: 'johnnydoe', password: 'johndoe123', email: 'johndoe@email.com'})
-tristan = User.create({first_name: 'Tristan', last_name: 'Mallorca', user_name: 'trrstn', password: 'trrstn123', email: 'trrstn@email.com'})
-humph = User.create({first_name: 'Humphrey', last_name: 'Litan', user_name: 'humphreylitan', password: 'humphreylitan', email: 'humphreylitan@email.com'})
-
-post = Post.create(
-  {
-    title: 'Tristan\'s Post',
-    body: 'Tristan\'s Post Body asdaf Lorem ipsum dolor sit amet, ne pri latine ornatus consequuntur, agam wisi referrentur usu ei. Ut vis enim tamquam, has eros dissentiet ad. Per cu etiam insolens, duo eu dictas phaedrum. Adhuc augue dissentiunt cu mel.',
-    user: tristan
-  })
-post2 = Post.create(
-  {
-    title: 'Look at this Post',
-    body: 'Ut eum porro iuvaret perfecto, quo agam semper malorum ut, mel no nisl aliquid omittam. Eos graece iuvaret ullamcorper ea, soleat erroribus comprehensam ne sit, ne facete eripuit qui.',
-    user: tristan
-  })
-post3 = Post.create(
-  {
-    title: 'The Big John Diary',
-    body: 'Qui id elit mundi scribentur, no audiam copiosae volutpat vis, in inani volumus expetendis vel.',
-    user: john
-  })
-post4 = Post.create(
-  {
-    title: 'Humphrey Post',
-    body: 'Tristan\'s Post Body asdaf Lorem ipsum dolor sit amet, ne pri latine ornatus consequuntur, agam wisi referrentur usu ei. Ut vis enim tamquam, has eros dissentiet ad. Per cu etiam insolens, duo eu dictas phaedrum. Adhuc augue dissentiunt cu mel.',
-    user: humph
-  })
-
-    lord = User.create({first_name: 'Admin', last_name: 'Lord', user_name: 'adminlord', password: 'adminlord', email: 'adminlord@email.com', admin: true})
+lord = User.create({first_name: 'Admin', last_name: 'Lord', user_name: 'adminlord', password: 'adminlord', email: 'adminlord@email.com', admin: true})
